3d8c358a5616b2a4c21ef5ea91986f4ff0e9e151
hisa <hisa@iporaitech.com>
2015-03-17 15:10:48 -0300

    Changes users index heading. Keeps conf for sqlite3. Adds config for postgres.

--- ror/db/schema.rb
+++ ror/db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150304192119) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
